14da799077013d06a88882a070717ff5cea363ac
Ezrean <alexandre.trillot@gmail.com>
2020-04-27 10:53:38 +0200

    bouton css

---
+++ add_evaluation.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<head>
+    <meta charset="utf-8">
+    <title>Ajouter Evaluation</title>
+    <link rel="stylesheet" type="text/css" href="css.css"/>
+    <link rel="stylesheet" type="text/css" href="add_evaluation.css"/>
+</head>
+<body>
+    <div class="all">
+        <div class="resumer_exo">
+            <table>
+                <H3>Résumer exercice</H3>
+            </table>
+        </div>
+        <div class="horraire">
+            <table>
+                <tr>
+                    <p>Date, heure de lancement</p>
+                    <p>Date, heure de fin</p>
+                    <input type="submit" name="valider_eval" value="Valider évaluation" >
+                </tr>
+            </table>
+        </div>
+        <div class="exercice">
+            <table>
+                <tr>
+                    <h3>Titre de l'excercie</h3> 
+                    <p>Consigne de L'exercice</p> 
+                    <input type="submit" name="add_evaluation" value="Ajouter exercice à l'évaluation">
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
