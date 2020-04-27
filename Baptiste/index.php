c367482bc2837411ea5373420819523c75bff004
Theobald <baptiste.theobald@outlook.fr>
2020-04-27 13:37:27 +0200

    :(

--- Baptiste/index.php
+++ Baptiste/index.php
@@ -1,8 +1,5 @@
 <?php
-<<<<<<< HEAD
-require('View/header.html')
-=======
-require('View/connexion.html');
-require('../Alexandre/View/add_evaluation.php');
->>>>>>> acf9bc5b9c3c020e2c79375943ede3bb57c9ec93
+
+require('View/header.html');
+
 ?>
