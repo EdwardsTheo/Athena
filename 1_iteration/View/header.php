ee16be12a7e45dd7c04038fc492bbfd2b8f89d90
MiA <mia@MacBook-Pro-de-technical.local>
2020-05-13 15:55:29 +0200

    mes fichiers

---
+++ 1_iteration/View/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../Public/styles/header.css"/>
+        <title>Header</title>
+    </head>
+    <body>
+        <section class ="main">
+        <header class="header">
+            <form action="#" class="form">
+                <a href="#" class="btn-text">Cours </a>
+                <a href="#" class="btn-text">Exercices </a>
+                <a href="#" class="btn-text">Profil</a>
+                <a href="#" class="btn-text">Evaluation </a>
+            </form>
+        </header>
+    </section>
+    </body>
+</html>
